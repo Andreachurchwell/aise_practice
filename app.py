b1b9d10bea8b19e5a99c8eab60fa39cbf618fa0a
Andreachurchwell <151082616+Andreachurchwell@users.noreply.github.com>
2025-10-27 18:24:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
+from calculator import add
+
+if __name__ == "__main__":
+    print("Welcome to Calculator App")
+    x = float(input("Enter first number: "))
+    y = float(input("Enter second number: "))
+    print(f"The sum is: {add(x, y)}")
 
