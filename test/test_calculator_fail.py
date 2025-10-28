f986097abeb982262cf7a7a8ad87d014877e723c
Andreachurchwell <151082616+Andreachurchwell@users.noreply.github.com>
2025-10-27 19:23:57 -0500

    Delete test directory

--- test/test_calculator_fail.py
+++
@@ -1,7 +0,0 @@
-from app.calculator import add, safe_divide
-import pytest
-def test_add_incorrect_on_purpose():
-  assert add(2,3) == 5
-def test_safe_divide_zero_error():
-  with pytest.raises(ZeroDivisionError):
-    safe_divide(10,0)
